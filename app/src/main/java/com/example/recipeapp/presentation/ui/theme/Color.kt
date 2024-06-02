c397e380e20cd6fd760f4b14afe5d3119697bdf2
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-02 15:58:27 +0300

    added app custom color

--- app/src/main/java/com/example/recipeapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/recipeapp/presentation/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val CardColor = Color(0xFFFFD42B)
+val ButtonColorPrimary = Color(0xFFE45151)
+val ButtonColorSecondary = Color(0xFFF6CACA)
+val BackgroundPrimary = Color(0xFFFFEEE9)
