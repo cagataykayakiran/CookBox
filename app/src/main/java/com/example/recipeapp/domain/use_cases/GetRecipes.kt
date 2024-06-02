6bd7f6c3bda6526d1f08b6d04514927ec97269a5
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-02 19:47:00 +0300

    fixed get recipes use case

--- app/src/main/java/com/example/recipeapp/domain/use_cases/GetRecipes.kt
+++ app/src/main/java/com/example/recipeapp/domain/use_cases/GetRecipes.kt
@@ -14,10 +14,10 @@ class GetRecipes @Inject constructor(
     private val repository: RecipeRepository,
     private val networkHelper: NetworkHelper
 ) {
-    operator fun invoke(diet: String): Flow<Resource<List<Recipe>>> = flow {
+    operator fun invoke(): Flow<Resource<List<Recipe>>> = flow {
         try {
-            emit(Resource.Loading())
-            val recipes = repository.getRecipes(diet)
+            emit(Resource.Loading(isLoading = true))
+            val recipes = repository.getRecipes()
             emit(Resource.Success(recipes))
         } catch (e: HttpException) {
             emit(Resource.Error(e.localizedMessage))
