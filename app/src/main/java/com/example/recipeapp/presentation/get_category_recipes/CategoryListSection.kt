9119d41996cbb6ef4d78c60babd39e3924770b98
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-02 19:43:11 +0300

    change package

--- app/src/main/java/com/example/recipeapp/presentation/get_category_recipes/CategoryListSection.kt
+++ app/src/main/java/com/example/recipeapp/presentation/get_category_recipes/CategoryListSection.kt
@@ -1,4 +1,4 @@
-package com.example.recipeapp.presentation.get_recipes
+package com.example.recipeapp.presentation.get_category_recipes
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -7,12 +7,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
@@ -25,11 +30,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.example.recipeapp.presentation.get_recipes.RecipeState
 import com.example.recipeapp.presentation.ui.theme.ButtonColorPrimary
 
 @Composable
 fun CategoryList(categories: List<String>, images: List<Int>) {
     var selectedIndex by remember { mutableIntStateOf(-1) }
+    val viewModel: RecipeTypeViewModel = hiltViewModel()
 
     LazyRow(
         verticalAlignment = Alignment.CenterVertically,
@@ -40,8 +49,59 @@ fun CategoryList(categories: List<String>, images: List<Int>) {
                 name = categories[index],
                 iconResId = images[index],
                 isSelected = index == selectedIndex,
-                onClick = { selectedIndex = index }
+                onClick = {
+                    selectedIndex = index
+                    viewModel.onEvent(UIEvent.SelectCategory(categories[index]))
+                }
             )
+        }
+    }
+}
+
+
+@Composable
+fun CategoryItemList(modifier: Modifier = Modifier, state: RecipeState) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        modifier = modifier.wrapContentSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        items(state.recipe) { recipe ->
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .size(75.dp, 110.dp)
+                    .background(Color.White)
+                    .clickable { },
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(Color.White)
+                            .size(60.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        AsyncImage(
+                            model = state.recipe.map { },
+                            contentDescription = null,
+                            modifier = Modifier.size(50.dp)
+                        )
+                    }
+                    Text(
+                        text = recipe.title,
+                        color = Color.Black,
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+            }
         }
     }
 }
@@ -51,7 +111,7 @@ fun CategoryItem(
     name: String,
     iconResId: Int,
     isSelected: Boolean,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     Box(
         modifier = Modifier
@@ -89,3 +149,4 @@ fun CategoryItem(
         }
     }
 }
+
