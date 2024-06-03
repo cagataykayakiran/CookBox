eac4d689583ea9b54ad6df9caf1b298cfa3df180
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-03 22:31:36 +0300

    deleted unused import

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.example.recipeapp.presentation.navigation.NavigationGraph
 import com.example.recipeapp.presentation.ui.theme.BackgroundPrimary
 import com.example.recipeapp.presentation.ui.theme.PaparaFinalCaseTheme
