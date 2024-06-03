d87cbfedc0b150e9316df0eefd73ca552a2d3003
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-03 22:22:17 +0300

    added search screen navigate composable

--- app/src/main/java/com/example/recipeapp/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/recipeapp/presentation/navigation/NavigationGraph.kt
@@ -6,19 +6,27 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.recipeapp.presentation.MainScreen
+import com.example.recipeapp.presentation.search_screen.presentation.SearchScreen
 import com.example.recipeapp.util.Screen
 
 @Composable
 fun NavigationGraph(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(modifier = modifier, navController = navController, startDestination = Screen.MainScreen.route) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Screen.MainScreen.route
+    ) {
         composable(route = Screen.MainScreen.route) {
             MainScreen(navController = navController)
         }
         composable(route = Screen.FavoriteScreen.route) {
         }
         composable(route = Screen.RecipeDetail.route) {
-            
+
+        }
+        composable(route = Screen.SearchScreen.route) {
+            SearchScreen()
         }
     }
 }
