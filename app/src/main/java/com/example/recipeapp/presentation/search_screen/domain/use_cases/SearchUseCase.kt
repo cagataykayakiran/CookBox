883f880ed4dd541c6f8423ca7ed0bca0250f71a6
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-03 22:20:39 +0300

    get recipe from api

---
+++ app/src/main/java/com/example/recipeapp/presentation/search_screen/domain/use_cases/SearchUseCase.kt
@@ -0,0 +1,26 @@
+package com.example.recipeapp.presentation.search_screen.domain.use_cases
+
+import com.example.myapplication.util.Resource
+import com.example.recipeapp.domain.model.Recipe
+import com.example.recipeapp.presentation.search_screen.data.SearchRepositoryImpl
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class SearchUseCase @Inject constructor(
+    private val searchRepositoryImpl: SearchRepositoryImpl
+) {
+    operator fun invoke(query: String): Flow<Resource<List<Recipe>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val recipes = searchRepositoryImpl.getRecipeByName(query)
+            emit(Resource.Success(recipes))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "Error"))
+        } catch (e: IOException) {
+            emit(Resource.Error(e.localizedMessage ?: "Error"))
+        }
+    }
+}
