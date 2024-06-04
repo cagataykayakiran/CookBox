be01eb747dae597c0a6bc685a2d9bead2afb176a
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-04 21:57:39 +0300

    added screen navigate

--- app/src/main/java/com/example/recipeapp/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/recipeapp/presentation/navigation/NavigationGraph.kt
@@ -2,15 +2,18 @@ package com.example.recipeapp.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.recipeapp.presentation.MainScreen
-import com.example.recipeapp.presentation.search_screen.presentation.SearchScreen
+import com.example.recipeapp.presentation.get_category_recipes.RecipeListScreenByCategoryScreen
+import com.example.recipeapp.presentation.get_category_recipes.RecipeTypeViewModel
+import com.example.recipeapp.presentation.search_screen.SearchScreen
 import com.example.recipeapp.util.Screen
 
 @Composable
-fun NavigationGraph(modifier: Modifier = Modifier) {
+fun NavigationGraph(modifier: Modifier = Modifier, viewModel: RecipeTypeViewModel = hiltViewModel()) {
     val navController = rememberNavController()
     NavHost(
         modifier = modifier,
@@ -27,6 +30,10 @@ fun NavigationGraph(modifier: Modifier = Modifier) {
         }
         composable(route = Screen.SearchScreen.route) {
             SearchScreen()
+        }
+        composable(route = Screen.RecipeListScreenByCategory.route + "/{category}") {
+            val category = it.arguments?.getString("category") ?: ""
+            RecipeListScreenByCategoryScreen(category = category, viewModel = viewModel, navController = navController)
         }
     }
 }
