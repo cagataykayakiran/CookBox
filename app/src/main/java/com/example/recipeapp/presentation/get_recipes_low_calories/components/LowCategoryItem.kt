26e814fa87bf7531825df9416f74750b3db94b0e
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-04 21:56:54 +0300

    created low calories ui and state

---
+++ app/src/main/java/com/example/recipeapp/presentation/get_recipes_low_calories/components/LowCategoryItem.kt
@@ -0,0 +1,69 @@
+package com.example.recipeapp.presentation.get_recipes_low_calories.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.recipeapp.domain.model.Recipe
+import com.example.recipeapp.presentation.ui.theme.MainColorSecondary
+import com.example.recipeapp.presentation.ui.theme.futuraSansFamily
+
+@Composable
+fun LowCategoryItem(modifier: Modifier = Modifier, recipe: Recipe) {
+    Box(
+        modifier = modifier
+            .padding(10.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .size(130.dp, 215.dp)
+            .background(MainColorSecondary)
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
+            ) {
+                AsyncImage(
+                    model = recipe.image,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 5.dp, start = 10.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = recipe.title,
+                    fontFamily = futuraSansFamily,
+                    maxLines = 2,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }
+}
