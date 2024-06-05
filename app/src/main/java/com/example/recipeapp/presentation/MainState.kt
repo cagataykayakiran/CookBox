e106569bd482ccf7a26338632d77961101a7a3c8
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-05 23:00:53 +0300

    created main state

---
+++ app/src/main/java/com/example/recipeapp/presentation/MainState.kt
@@ -0,0 +1,45 @@
+package com.example.recipeapp.presentation
+
+import com.example.recipeapp.domain.model.Recipe
+
+sealed interface MainState {
+    val data: List<Recipe>
+    val isLoading: Boolean
+    val error: String
+    data class CardRecipeState(
+        override val data: List<Recipe> = emptyList(),
+        override val isLoading: Boolean = false,
+        override val error: String = ""
+    ) : MainState
+
+    data class RecipeTypeState(
+        override val data: List<Recipe> = emptyList(),
+        override val isLoading: Boolean = false,
+        override val error: String = ""
+    ) : MainState
+
+    data class LowCaloriesState(
+        override val data: List<Recipe> = emptyList(),
+        override val isLoading: Boolean = false,
+        override val error: String = ""
+    ): MainState
+
+    data class SearchState(
+        override val data: List<Recipe> = emptyList(),
+        override val isLoading: Boolean = false,
+        override val error: String = "",
+        val searchQuery: String = "",
+    ): MainState
+
+    data class MaxReadyTimeState(
+        override val data: List<Recipe> = emptyList(),
+        override val isLoading: Boolean = false,
+        override val error: String = "",
+    ): MainState
+
+    data class HighProteinState(
+        override val data: List<Recipe> = emptyList(),
+        override val isLoading: Boolean = false,
+        override val error: String = "",
+    ): MainState
+}
