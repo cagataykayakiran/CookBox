c862610e654a1e4a33385cdbc36dde9a2475ed9d
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-05 22:56:44 +0300

    added nav controller and change icon

--- app/src/main/java/com/example/recipeapp/presentation/components/AppBottomBar.kt
+++ app/src/main/java/com/example/recipeapp/presentation/components/AppBottomBar.kt
@@ -2,8 +2,10 @@ package com.example.recipeapp.presentation.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -13,54 +15,69 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.recipeapp.presentation.ui.theme.MainColorPrimary
 import com.example.recipeapp.presentation.ui.theme.MainColorSecondary
 import com.example.recipeapp.presentation.ui.theme.futuraSansFamily
+import com.example.recipeapp.util.Screen
 
 
 data class BottomNavigationItem(
     val title: String,
+    val route: Screen,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
 )
 
 @Composable
-fun AppBottomBar() {
+fun AppBottomBar(navController: NavController) {
     val items = listOf(
         BottomNavigationItem(
             title = "Home",
             selectedIcon = Icons.Filled.Home,
             unselectedIcon = Icons.Outlined.Home,
+            route = Screen.MainScreen
         ),
         BottomNavigationItem(
             title = "Favorites",
-            selectedIcon = Icons.Filled.Email,
-            unselectedIcon = Icons.Outlined.Email,
+            selectedIcon = Icons.Filled.Favorite,
+            unselectedIcon = Icons.Outlined.Favorite,
+            route = Screen.FavoriteScreen
         )
     )
-    var selectedItemByIndex by rememberSaveable { mutableIntStateOf(0) }
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+    var selectedIndex by rememberSaveable { mutableIntStateOf(0) }
+    selectedIndex = items.indexOfFirst { it.route.route == currentRoute }
     NavigationBar(
         containerColor = Color.White
     ) {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
-                selected = selectedItemByIndex == index,
-                onClick = { selectedItemByIndex = index },
+                selected = selectedIndex == index,
+                onClick = {
+                    selectedIndex = index
+                    navController.navigate(item.route.route) {
+                        launchSingleTop = true
+                    }
+                },
                 icon = {
                     Icon(
-                        imageVector = if (index == selectedItemByIndex) {
+                        imageVector = if (index == selectedIndex) {
                             item.selectedIcon
                         } else item.unselectedIcon, contentDescription = null
                     )
                 },
                 label = {
-                    if (selectedItemByIndex == index) {
+                    if (selectedIndex == index) {
                         Text(
                             text = item.title,
                             fontSize = 12.sp,
