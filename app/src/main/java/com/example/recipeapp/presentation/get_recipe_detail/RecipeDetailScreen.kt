1e695a326ea3db9653d85be0a692298bd2731598
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-05 22:59:10 +0300

    created new screen

---
+++ app/src/main/java/com/example/recipeapp/presentation/get_recipe_detail/RecipeDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.example.recipeapp.presentation.get_recipe_detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecipeDetailScreen(modifier: Modifier = Modifier) {
+
+}
