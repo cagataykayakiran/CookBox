676f1a63113d8798125a5e708c9a58f5e48f95c2
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-05 23:03:12 +0300

    bug fix

--- app/src/main/java/com/example/recipeapp/presentation/MainScreen.kt
+++ app/src/main/java/com/example/recipeapp/presentation/MainScreen.kt
@@ -3,7 +3,6 @@ package com.example.recipeapp.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,21 +12,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.recipeapp.presentation.card_slider_section.CardSliderSection
+import com.example.recipeapp.presentation.get_recipes_card.CardSliderSection
 import com.example.recipeapp.presentation.components.AppBottomBar
 import com.example.recipeapp.presentation.components.AppSearchBar
 import com.example.recipeapp.presentation.components.AppTopBar
 import com.example.recipeapp.presentation.get_category_recipes.CategorySection
 import com.example.recipeapp.presentation.get_category_recipes.RecipeTypeViewModel
-import com.example.recipeapp.presentation.get_recipes.RecipeViewModel
-import com.example.recipeapp.presentation.get_recipes_low_calories.LowCategorySection
+import com.example.recipeapp.presentation.get_recipes_card.RecipeCardViewModel
+import com.example.recipeapp.presentation.get_recipes_high_protein.HighProteinSection
+import com.example.recipeapp.presentation.get_recipes_low_calories.LowCaloriesSection
+import com.example.recipeapp.presentation.get_recipes_low_ready_time.LowReadyTimeCategorySection
+import com.example.recipeapp.presentation.ui.theme.BackgroundPrimary
 import com.example.recipeapp.presentation.ui.theme.futuraSansFamily
 
 
@@ -35,67 +35,74 @@ import com.example.recipeapp.presentation.ui.theme.futuraSansFamily
 fun MainScreen(
     navController: NavController,
     modifier: Modifier = Modifier,
-    viewModel: RecipeViewModel = hiltViewModel(),
-    typeViewModel: RecipeTypeViewModel = hiltViewModel()
+    recipeCardViewModel: RecipeCardViewModel = hiltViewModel(),
+    typeViewModel: RecipeTypeViewModel = hiltViewModel(),
 ) {
-    val mainState by viewModel.recipesState.collectAsState()
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         topBar = { AppTopBar() },
-        bottomBar = { AppBottomBar() },
-        containerColor = Color.White
+        bottomBar = { AppBottomBar(navController = navController) },
+        containerColor = BackgroundPrimary
     ) { innerPadding ->
         LazyColumn(
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
-                .padding(horizontal = 20.dp, vertical = 17.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
+                .padding(15.dp)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(15.dp)
         ) {
             item {
                 AppSearchBar(navController = navController)
-                Spacer(modifier = Modifier.height(10.dp))
+            }
+            item {
                 Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 4.dp, start = 7.dp),
                     text = "Amazing Breakfast",
-                    textAlign = TextAlign.Left,
-                    fontWeight = FontWeight.SemiBold,
+                    fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
                     fontFamily = futuraSansFamily
                 )
                 CardSliderSection(
                     navController = navController,
-                    cardItems = mainState.recipe,
+                    recipeCardViewModel = recipeCardViewModel
                 )
-                Spacer(modifier = Modifier.height(10.dp))
+            }
+            item {
                 Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 4.dp, start = 7.dp),
                     text = "Categories",
-                    textAlign = TextAlign.Left,
-                    fontWeight = FontWeight.SemiBold,
+                    fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
                     fontFamily = futuraSansFamily
                 )
-                Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(8.dp))
                 CategorySection(typeViewModel = typeViewModel, navController = navController)
-                Spacer(modifier = Modifier.height(10.dp))
+            }
+            item {
                 Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 4.dp, start = 7.dp),
                     text = "Low Calories ",
-                    textAlign = TextAlign.Left,
-                    fontWeight = FontWeight.SemiBold,
+                    fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
                     fontFamily = futuraSansFamily
                 )
-                Spacer(modifier = Modifier.height(10.dp))
-                LowCategorySection()
+                LowCaloriesSection()
+            }
+            item {
+                Text(
+                    text = "Low Ready Time",
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp,
+                    fontFamily = futuraSansFamily
+                )
+                LowReadyTimeCategorySection()
+            }
+            item {
+                Text(
+                    text = "High Protein",
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp,
+                    fontFamily = futuraSansFamily
+                )
+                HighProteinSection()
             }
         }
     }
