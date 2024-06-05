ef10990f8e865e17ab991337e7c408bafd64a728
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-05 23:00:27 +0300

    created category section

---
+++ app/src/main/java/com/example/recipeapp/presentation/get_recipes_low_calories/LowCaloriesSection.kt
@@ -0,0 +1,28 @@
+package com.example.recipeapp.presentation.get_recipes_low_calories
+
+
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.recipeapp.presentation.components.ItemList
+
+@Composable
+fun LowCaloriesSection(
+    modifier: Modifier = Modifier,
+    lowCaloriesViewModel: LowCaloriesViewModel = hiltViewModel()
+) {
+
+    val lowCaloriesState by lowCaloriesViewModel.lowCaloriesState.collectAsState()
+
+    LazyRow(
+        modifier = modifier,
+    ) {
+        items(lowCaloriesState.data) { recipe ->
+            ItemList(recipe = recipe)
+        }
+    }
+}
--- app/src/main/java/com/example/recipeapp/presentation/get_recipes_low_calories/LowCaloriesViewModel.kt
+++ app/src/main/java/com/example/recipeapp/presentation/get_recipes_low_calories/LowCaloriesViewModel.kt
@@ -1,61 +1,50 @@
 package com.example.recipeapp.presentation.get_recipes_low_calories
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.myapplication.util.Resource
+import com.example.recipeapp.util.Resource
 import com.example.recipeapp.domain.use_cases.GetRecipesByLowCalories
-import com.example.recipeapp.domain.use_cases.GetTypeByRecipes
-import com.example.recipeapp.presentation.get_category_recipes.RecipeTypeState
-import com.example.recipeapp.presentation.get_category_recipes.UIEvent
+import com.example.recipeapp.presentation.MainState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class LowCaloriesViewModel @Inject constructor(
-    private val getRecipesByLowCalories: GetRecipesByLowCalories
+    private val getRecipesByLowCalories: GetRecipesByLowCalories,
 ): ViewModel() {
 
-    private val _lowCaloriesState = MutableStateFlow(LowCaloriesState())
+    private val _lowCaloriesState = MutableStateFlow(MainState.LowCaloriesState())
     val lowCaloriesState = _lowCaloriesState.asStateFlow()
 
     init {
         getLowCalories()
     }
+
     private fun getLowCalories() {
         getRecipesByLowCalories().onEach { result ->
             when(result) {
                 is Resource.Success -> {
-                    _lowCaloriesState.update {
-                        it.copy(
-                            recipe = result.data ?: emptyList(),
-                        )
-                    }
+                    _lowCaloriesState.value = _lowCaloriesState.value.copy(
+                        data = result.data ?: emptyList()
+                    )
                 }
                 is Resource.Loading -> {
-                    _lowCaloriesState.update {
-                        it.copy(
-                            isLoading = true
-                        )
-                    }
+                    _lowCaloriesState.value = _lowCaloriesState.value.copy(
+                        isLoading = true
+                    )
                 }
                 is Resource.Error -> {
-                    _lowCaloriesState.update {
-                        it.copy(
-                            error = result.message ?: "Error",
-                        )
-                    }
-                }
-
-                else -> {
-                    println("Error")
+                    _lowCaloriesState.value = _lowCaloriesState.value.copy(
+                        error = result.message ?: "Error!"
+                    )
                 }
             }
         }.launchIn(viewModelScope)
     }
-
 }
