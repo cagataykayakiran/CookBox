9a54a838dbc87c4d02f8febfc5ca9b6ef4312830
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-05 22:52:40 +0300

    deleted

--- app/src/main/java/com/example/recipeapp/presentation/get_recipes_low_calories/LowCaloriesState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recipeapp.presentation.get_recipes_low_calories
-
-import com.example.recipeapp.domain.model.Recipe
-
-data class LowCaloriesState(
-    val isLoading: Boolean = false,
-    val recipe: List<Recipe> = emptyList(),
-    val error: String = ""
-)
