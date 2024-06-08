9ebdfc50ad0795fef5f5fa18e07911caf35ebb67
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-08 18:27:49 +0300

    created recipe detail dto

---
+++ app/src/main/java/com/example/recipeapp/data/remote/response/MeasuresDto.kt
@@ -0,0 +1,6 @@
+package com.example.recipeapp.data.remote.response
+
+data class MeasuresDto(
+    val usDto: UsDto?,
+    val metricDto: MetricDto?
+)
---
+++ app/src/main/java/com/example/recipeapp/data/remote/response/MetricDto.kt
@@ -0,0 +1,7 @@
+package com.example.recipeapp.data.remote.response
+
+data class MetricDto(
+    val amount: Double?,
+    val unitShort: String?,
+    val unitLong: String?
+)
