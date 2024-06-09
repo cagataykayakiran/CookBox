9d25bcf9724bfabf3bf4c090fa68c461b17da50c
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-09 15:29:30 +0300

    created entities

---
+++ app/src/main/java/com/example/recipeapp/data/local/IngredientEntity.kt
@@ -0,0 +1,28 @@
+package com.example.recipeapp.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.recipeapp.domain.model.ExtendedIngredient
+
+@Entity(tableName = "ingredients")
+data class IngredientEntity(
+    @PrimaryKey
+    val id: Int,
+    val recipeId: Int,
+    val nameClean: String,
+    val amount: Double,
+    val image: String,
+    val name: String,
+    val original: String
+)
+
+fun IngredientEntity.toExtendedIngredient(): ExtendedIngredient {
+    return ExtendedIngredient(
+        id = id,
+        nameClean = nameClean,
+        amount = amount,
+        image = image,
+        name = name,
+        original = original
+    )
+}
---
+++ app/src/main/java/com/example/recipeapp/data/local/RecipeEntity.kt
@@ -0,0 +1,71 @@
+package com.example.recipeapp.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.recipeapp.domain.model.ExtendedIngredient
+import com.example.recipeapp.domain.model.Recipe
+import com.example.recipeapp.domain.model.RecipeDetail
+
+@Entity(tableName = "recipes")
+data class RecipeEntity(
+    @PrimaryKey
+    val mainId: Int,
+    val title: String,
+    val image: String,
+    val vegetarian: Boolean,
+    val vegan: Boolean,
+    val glutenFree: Boolean,
+    val dairyFree: Boolean,
+    val veryHealthy: Boolean,
+    val cheap: Boolean,
+    val veryPopular: Boolean,
+    val sustainable: Boolean,
+    val weightWatcherSmartPoints: Int,
+    val aggregateLikes: Int,
+    val healthScore: Int,
+    val pricePerServing: Double,
+    val readyInMinutes: Int,
+    val servings: Int,
+    val sourceUrl: String,
+    val summary: String,
+    val instructions: String,
+    val dishTypes: String,
+    val isFavorite: Boolean = false,
+)
+
+fun RecipeEntity.toRecipe(): Recipe {
+    return Recipe(
+        id = mainId,
+        title = title,
+        image = image,
+    )
+}
+
+fun RecipeEntity.toRecipeDetail(ingredients: List<ExtendedIngredient>): RecipeDetail {
+    return RecipeDetail(
+        id = mainId,
+        title = title,
+        image = image,
+        vegetarian = vegetarian,
+        vegan = vegan,
+        glutenFree = glutenFree,
+        dairyFree = dairyFree,
+        veryHealthy = veryHealthy,
+        cheap = cheap,
+        veryPopular = veryPopular,
+        sustainable = sustainable,
+        weightWatcherSmartPoints = weightWatcherSmartPoints,
+        aggregateLikes = aggregateLikes,
+        healthScore = healthScore,
+        pricePerServing = pricePerServing,
+        readyInMinutes = readyInMinutes,
+        servings = servings,
+        sourceUrl = sourceUrl,
+        summary = summary,
+        extendedIngredients = ingredients,
+        instructions = instructions,
+        dishTypes = dishTypes.split(","),
+        isFavorite = isFavorite
+    )
+}
+
