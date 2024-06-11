c454e53791bb115500c83c39562072187acee52d
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-11 13:09:16 +0300

    created work classes and module

--- app/src/main/java/com/example/recipeapp/RecipeApp.kt
+++ app/src/main/java/com/example/recipeapp/RecipeApp.kt
@@ -1,8 +1,14 @@
 package com.example.recipeapp
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
-
+import javax.inject.Inject
 @HiltAndroidApp
-class RecipeApp: Application() {
+class RecipeApp: Application(), Configuration.Provider {
+
+
+    @Inject lateinit var workerConfiguration: Configuration
+    override val workManagerConfiguration: Configuration
+        get() = workerConfiguration
 }
