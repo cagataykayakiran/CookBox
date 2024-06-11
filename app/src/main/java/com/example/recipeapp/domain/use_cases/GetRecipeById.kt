94a63778c3a5a3027ba3bf14a614166185d7740c
cagataykayakiran <cagataykayakiran@gmail.com>
2024-06-11 13:13:44 +0300

    check internet connection

--- app/src/main/java/com/example/recipeapp/domain/use_cases/GetRecipeById.kt
+++ app/src/main/java/com/example/recipeapp/domain/use_cases/GetRecipeById.kt
@@ -1,26 +1,48 @@
 package com.example.recipeapp.domain.use_cases
 
+import com.example.recipeapp.data.local.entity.toExtendedIngredient
+import com.example.recipeapp.data.local.entity.toRecipeDetail
 import com.example.recipeapp.domain.model.RecipeDetail
 import com.example.recipeapp.domain.repository.RecipeRepository
+import com.example.recipeapp.util.NetworkHelper
 import com.example.recipeapp.util.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 class GetRecipeById @Inject constructor(
     private val repository: RecipeRepository,
+    private val networkHelper: NetworkHelper
 ) {
     operator fun invoke(id: Int): Flow<Resource<RecipeDetail>> = flow {
-        try {
-            emit(Resource.Loading())
-            val recipe = repository.getRecipesById(id)
-            emit(Resource.Success(recipe))
-        } catch (e: HttpException) {
-            emit(Resource.Error(e.localizedMessage ?: "Error"))
-        } catch (e: IOException) {
-            emit(Resource.Error(e.localizedMessage ?: "Error"))
+        emit(Resource.Loading())
+        if (networkHelper.isNetworkConnected()) {
+            try {
+                val recipe = repository.getRecipesById(id)
+                emit(Resource.Success(recipe))
+            } catch (e: Exception) {
+                val recipeDetail = repository.getRecipeWithIngredientsById(id)
+                if (recipeDetail != null) {
+                    emit(Resource.Success(
+                        recipeDetail.recipe.toRecipeDetail(
+                            recipeDetail.ingredients.map { it.toExtendedIngredient() }
+                        )
+                    ))
+                } else {
+                    emit(Resource.Error("Unexpected error!"))
+                }
+            }
+        } else {
+            val recipeDetail = repository.getRecipeWithIngredientsById(id)
+            if (recipeDetail != null) {
+                emit(Resource.Success(
+                    recipeDetail.recipe.toRecipeDetail(
+                        recipeDetail.ingredients.map { it.toExtendedIngredient() }
+                    )
+                ))
+            } else {
+                emit(Resource.Error("Unexpected error!"))
+            }
         }
     }
 }
